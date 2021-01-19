a0584ededdb4bc43388316428b4f36f10415ef9c
Clara Nascimento <clara.nascimento@outlook.com>
2021-01-19 20:11:43 -0300

    119.1 - Create project, add images in Assets, add image in LaunchScreen.

---
+++ MovieQuiz/MovieQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Clara Nascimento on 19/01/21.
+//  Copyright © 2021 Clara Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
