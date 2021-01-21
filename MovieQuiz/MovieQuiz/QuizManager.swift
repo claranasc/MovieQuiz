7acc90ba315bcc838c3609025ac71086a382490b
Clara Nascimento <clara.nascimento@outlook.com>
2021-01-21 19:34:16 -0300

    120.1 - Create QuizManager, add class QuizManager, add struct Quis and add struct QuizOption.

---
+++ MovieQuiz/MovieQuiz/QuizManager.swift
@@ -0,0 +1,22 @@
+//
+//  QuizManager.swift
+//  MovieQuiz
+//
+//  Created by Clara Nascimento on 21/01/21.
+//  Copyright © 2021 Clara Nascimento. All rights reserved.
+//
+
+import Foundation
+
+class QuizManager {
+    
+}
+
+struct Quiz: Codable {
+    let name: String
+    let number: Int
+}
+
+struct QuizOption: Codable {
+    let name: String
+}
