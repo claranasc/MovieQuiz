1161bfea877b2684e11c3743480d74a7359582fc
Clara Nascimento <clara.nascimento@outlook.com>
2021-01-21 20:34:03 -0300

    120.5 - In ViewController: add var quizManager: QuizManager!, add verride func viewWillAppear(_ animated: Bool), add func getNewQuiz(), Implemented @IBAction func checkAnswer and @IBAction func showHideSoundBar.

--- MovieQuiz/MovieQuiz/ViewController.swift
+++ MovieQuiz/MovieQuiz/ViewController.swift
@@ -14,25 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var slMusic: UISlider!
     @IBOutlet var btOptions: [UIButton]!
     @IBOutlet weak var ivQuiz: UIImageView!
-        
+    @IBOutlet weak var viTimer: UIView!
     
-    @IBAction func checkAnswer(_ sender: UIButton) {
-    }
-    
-    
-    
-    
-    
+    var quizManager: QuizManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        quizManager = QuizManager()
+        getNewQuiz()
+    }
+    
+    func getNewQuiz() {
+        let round = quizManager.generateRandomQuinz()
+        for i in 0..<round.options.count {
+            btOptions[i].setTitle(round.options[i].name, for: .normal)
+        }
+    }
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        quizManager.checkAnswer(sender.title(for: .normal)!)
+        getNewQuiz()
+    }
+    
     @IBAction func changeMusicTime(_ sender: UISlider) {
     }
     
     @IBAction func showHideSoundBar(_ sender: UIButton) {
+        viSoundBar.isHidden = !viSoundBar.isHidden
     }
 
     @IBAction func changeMusicStatus(_ sender: UIButton) {
