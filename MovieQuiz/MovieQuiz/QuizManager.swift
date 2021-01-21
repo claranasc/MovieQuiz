3ca313add2ed0d318997f908bd03391e4dd777fd
Clara Nascimento <clara.nascimento@outlook.com>
2021-01-21 19:44:34 -0300

    120.2 - Implemented class QuizManager.

--- MovieQuiz/MovieQuiz/QuizManager.swift
+++ MovieQuiz/MovieQuiz/QuizManager.swift
@@ -10,6 +10,25 @@ import Foundation
 
 class QuizManager {
     
+    let quizes: [Quiz]
+    let quizOptions: [QuizOption]
+    var score: Int
+    
+    init() {
+        score = 0
+        let quizesURL = Bundle.main.url(forResource: "quizes", withExtension: "json")!
+        do {
+            let quizesData = try Data(contentsOf: quizesURL)
+            quizes = try JSONDecoder().decode([Quiz].self, from: quizesData)
+            let quizOptionsURL = Bundle.main.url(forResource: "options", withExtension: "json")!
+            let quizOptionsData = try! Data(contentsOf: quizOptionsURL)
+            quizOptions = try! JSONDecoder().decode([QuizOption].self, from: quizOptionsData)
+        } catch {
+            print(error.localizedDescription)
+            quizes = []
+            quizOptions = []
+        }
+    }
 }
 
 struct Quiz: Codable {
