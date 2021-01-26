c4123b4ef484ddda9b60f42b2e35c24b7c5c3593
Clara Nascimento <clara.nascimento@outlook.com>
2021-01-26 20:22:37 -0300

    122.4 - Add var score in GameOverViewController; Add override func prepare(for segue:.... In ViewController.

--- MovieQuiz/MovieQuiz/GameOverViewController.swift
+++ MovieQuiz/MovieQuiz/GameOverViewController.swift
@@ -12,18 +12,15 @@ class GameOverViewController: UIViewController {
 
     @IBOutlet weak var lbScore: UILabel!
     
-    
+    var score: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lbScore.text = "\(score)"
     }
     
     @IBAction func playAgain(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
-    
-    
-    
-    
-    
 }
